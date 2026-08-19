e89a6f5a244437de42cdc9f12b8951c0d93fa6b9
Hussain1u2 <navsariwalahussain5253@gmail.com>
2026-08-19 16:22:37 +0530

    plotly error solve

--- Full_MPR genration_app.py
+++ Full_MPR genration_app.py
@@ -9,8 +9,12 @@ from openpyxl.styles import Alignment, Border, Font, PatternFill, Side
 from openpyxl.utils import get_column_letter
 from openpyxl.worksheet.table import Table, TableStyleInfo
 import streamlit as st
-import plotly.express as px
-import plotly.graph_objects as go
+try:
+    import plotly.express as px
+    import plotly.graph_objects as go
+    HAS_PLOTLY = True
+except ImportError:
+    HAS_PLOTLY = False
 
 
 # Styling constants for OpenPyXL excel generation
